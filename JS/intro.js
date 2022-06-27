9fec3ab4c6d0e84b47a79f40c533f4c3ecc7a002
digitaldeja0 <90660363+digitaldeja0@users.noreply.github.com>
2022-06-27 06:21:31 -0400

    Add JS intro.

---
+++ JS/intro.js
@@ -0,0 +1,35 @@
+"What is a javascript comment? How do you create them? What is there purpose?";
+"Javascript comments are notes. Javascript comments are created with two // keys. Javascript comments are note for ourselved or code readers";
+// This is a javascript comment
+"// This is a javascript comment";
+
+"How do you find out what data type a piece of data is under classification?";
+"you can use the 'typeof' ";
+// Example of using type of: typeof "cat"
+// Example Result: "String"
+"typeof 'cat'";
+"'String'";
+
+"What is camel case?";
+"A naming convention javascript developers use to create a variable with more than one name.";
+// This is camel case: iAmCamelCase
+"iAmCamelCase";
+
+"What happens when you create a varible without an equal operator to assign it value?";
+"You get 'undefined'";
+// Example: let cat; console.log(cat);
+//"undefined"
+"let cat; console.log(cat);";
+"undefined";
+
+"What happenens when you concatenate two varibales: string + string, number + number, string + number?";
+" string + string will smush the two strings into one string, number + number will add the two numerical values, string + number will smush the two varibles together to create one string, numbers will be treated as a string. ";
+"no example";
+"no result";
+
+"True of false, you can set a varible equal to the result of a  mathmatical or comparison equation?";
+"True";
+"let myNumber = 1 + 2; or let myNumber = myNumber === 3";
+
+"While there are 7 data types used in javascript, what are the 4 most commmonly used primitives?";
+"Strings, Number, Boolean, Undefined";
